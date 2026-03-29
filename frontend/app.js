0de31b00244a15e85fd8b29cbec3dc45917e6980
Roman Andreev <andreev.grizzly@gmail.com>
2026-03-29 23:33:54 +0300

    small fix

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const apiBase = `${window.location.protocol}//${window.location.hostname}:8000/api`;
+const apiBase = `${window.location.protocol}//${window.location.hostname}:8888/api`;
 
 async function request(path, options = {}) {
   const res = await fetch(`${apiBase}${path}`, {
